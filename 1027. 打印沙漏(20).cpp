e1f87cdf92d6c718fc5096ac36b56fc6c6bb83d4
MrsW6 <1070624384@qq.com>
2018-04-14 14:52:22 +0800

    Code

--- 1027. 打印沙漏(20).cpp
+++ 1027. 打印沙漏(20).cpp
@@ -7,47 +7,80 @@ int main()
 	char c;
 	cin >> n >> c;
 	
-	int cengShu = 0;
-	int count = 1;
-	int realCount;
-	
-	while (count <= n) {
-		cengShu++;
-		realCount = count;
-		count = count + (count + 2) * 2;
+	if (n <= 6)
+	{
+		cout << c << endl;
+		cout << n - 1;
 	}
+	else
+	{
+		//先判断有几层
 	
-	int kongGe;
-	int realCengShu = cengShu;
-	
-	//上半层 
-	for (int i = cengShu; i >= 1; i--) {
+		//x为最大的星星行是几个星星 
+		int x = 1;
+		//count是一共多少个星星 
+		int count = 1;
+		//上层行数
+		int h = 0; 
+		int realCount = 1;
+		while (count <= n) {
+			h++;
+			x += 2;
+			realCount = count;
+			count = count + (x + 2) * 2;
+		} 
+		//cout << "x is " << x << " h is " << h << " realCount is " << realCount;
 		
-		//打印空格 
-		int kongGe = cengShu - realCengShu;
-		while(kongGe--) {
-			cout << " ";
+		//上半层 	
+		//星星前面空格数 
+		int kgs = 0;
+		//i代表每一行的星星数 
+		int i = x;
+		while (i >= 3) {
+			int xnkgs = kgs;
+			while(xnkgs--) {
+				cout << " ";
+			}
+			kgs++;
+			
+			int m = i;
+			while(m--) {
+				cout << c;
+			}
+			cout << endl;
+			i = i - 2;
 		}
 		
-		//打印星星
-		int q = 2 * realCengShu + 1;
-		while (q -- ) {
-			cout << c ;
-		} 
-		 
-		cout << endl;
-		realCengShu --;
-	}
-	
-	while(cengShu--){
-		cout << " ";
-	}
-	cout << c;
-	
-	int kongYu = 1;
-	int yuyuecengshu = 1;
-	while (yuyuecengshu < cengShu) {
-		cout << " ";
-	}
+		//中间的星星
+		int xnh = h;
+		while(xnh--) {
+			cout << " ";
+		}
+		cout << c << endl;
+		
+		//下半层
+		int xxnh = h;
+		
+		int q = 3;
+		int xxxnh = h;
+		while(xxnh--) {
+			
+			xxxnh--;
+			int mh = xxxnh;
+			while(mh--) {
+				cout << " ";
+			}
+			
+			
+			for(int i = 1; i <= q; i++) {
+				cout << c;
+			}
+			cout << endl;
+			q += 2;
+		}
+		int rrr;
+		rrr = 2 * h * (h + 2) + 1;
+		cout << n - rrr;
+		}
 }
 
